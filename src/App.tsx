d89f51c09ef57be90f4c309edc6112cc7358c2ca
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-01-18 01:12:49 +0900

    Implement a basic display

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import BingoBoard from "./components/BingoBoard";
+import Button from "./components/Button";
 import styles from "./App.module.css";
 
 export default function App() {
   return (
     <main className={styles.main}>
       <h1>
-        <span>D</span>
-        <span>A</span>
-        <span>研</span>
+        <span>社</span>
+        <span>情</span>
         <span>B</span>
         <span>I</span>
         <span>N</span>
@@ -15,6 +15,20 @@ export default function App() {
         <span>O</span>
       </h1>
       <BingoBoard />
+      <div className={styles.buttonContainer}>
+        <Button
+          label={"はじめから"}
+          onClick={() => {
+            window.location.reload();
+          }}
+        />
+        <Button
+          label={"まわす"}
+          onClick={() => {
+            alert("Not implemented!");
+          }}
+        />
+      </div>
     </main>
   );
 }
--- src/components/BingoBoard.tsx
+++ src/components/BingoBoard.tsx
@@ -1,5 +1,26 @@
 import styles from "./BingoBoard.module.css";
+import items from "../data/items.json";
 
 export default function BingoBoard() {
-  return <div className={styles.boardRoot}></div>;
+  return (
+    <div className={styles.boardRoot}>
+      <table className={styles.table}>
+        <thead>
+          {Array(5)
+            .fill(0)
+            .map((_, i) => {
+              return (
+                <tr key={i}>
+                  {Array(15)
+                    .fill(0)
+                    .map((_, j) => {
+                      return <td key={j}>{items[i * 15 + j]}</td>;
+                    })}
+                </tr>
+              );
+            })}
+        </thead>
+      </table>
+    </div>
+  );
 }
