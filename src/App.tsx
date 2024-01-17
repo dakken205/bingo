b5a999234a0d2ce11df8b56a3bec8e02994a0b22
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-01-18 03:50:51 +0900

    Implement a drawing animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
 import BingoBoard from "./components/BingoBoard";
 import Button from "./components/Button";
+import Drawing from "./components/Drawing";
 import items from "./data/items.json";
+import drum from "./assets/se/short.mp3";
+import closing from "./assets/se/closing.mp3";
 import styles from "./App.module.css";
 
 export default function App() {
+  const [isAnimationEnabled, setIsAnimationEnabled] = useState<boolean>(false);
   const [drawn, setDrawn] = useState<Set<number>>(new Set());
+  const [isDrawing, setIsDrawing] = useState<boolean>(false);
 
   const draw = () => {
     if (drawn.size === items.length) return;
     let remaining = Array.from(items.keys()).filter((i) => !drawn.has(i));
     let index = Math.floor(Math.random() * remaining.length);
     let item = remaining[index];
-    drawn.add(item);
-    setDrawn((prev) => new Set(prev.add(item)));
+
+    if (isAnimationEnabled) {
+      setIsDrawing(true);
+      new Audio(drum).play();
+      setTimeout(() => {
+        new Audio(closing).play();
+        setIsDrawing(false);
+        drawn.add(item);
+        setDrawn((prev) => new Set(prev.add(item)));
+      }, 5000);
+    } else {
+      drawn.add(item);
+      setDrawn((prev) => new Set(prev.add(item)));
+    }
   };
 
   return (
     <main className={styles.main}>
+      {isDrawing && <Drawing />}
       <h1>
         <span>社</span>
         <span>情</span>
@@ -36,6 +54,15 @@ export default function App() {
           }}
         />
         <Button label={"まわす"} onClick={draw} />
+      </div>
+      <div className={styles.animationButtonContainer}>
+        <input
+          type="checkbox"
+          id="animation"
+          checked={isAnimationEnabled}
+          onChange={(e) => setIsAnimationEnabled(e.target.checked)}
+        />
+        <label htmlFor="animation">サウンドとアニメーション</label>
       </div>
     </main>
   );
---
+++ src/components/Drawing.tsx
@@ -0,0 +1,9 @@
+import styles from "./Drawing.module.css";
+
+export default function Drawing() {
+  return (
+    <div className={styles.drawingRoot}>
+      <h1>抽選中</h1>
+    </div>
+  );
+}
